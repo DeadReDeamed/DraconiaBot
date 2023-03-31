f895164cb4a125536175eed9a7ab208b8e55e181
Luca <luca.1999@hotmail.com>
2023-03-31 09:18:33 +0200

    Added readme and updated workflow

--- Readme.mk
+++ Readme.mk
@@ -1,1 +1,6 @@
-test
+https://img.shields.io/github/actions/workflow/status/DeadReDeamed/DraconiaBot/main.yml
+
+https://img.shields.io/github/v/release/DeadReDeamed/DraconiaBot
+
+# Draconia bot
+This is a discord bot created for a RPG experience in discord.
