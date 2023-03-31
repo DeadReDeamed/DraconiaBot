8c546f87855c1f9fa818c0f7aa86e19188568747
Luca <luca.1999@hotmail.com>
2023-03-31 09:20:22 +0200

    fixed readme

--- Readme.mk
+++ Readme.mk
@@ -1,6 +1,7 @@
-https://img.shields.io/github/actions/workflow/status/DeadReDeamed/DraconiaBot/main.yml
+<img src="https://img.shields.io/github/actions/workflow/status/DeadReDeamed/DraconiaBot/main.yml" alt="buildstatus"/>
 
-https://img.shields.io/github/v/release/DeadReDeamed/DraconiaBot
+<img src="https://img.shields.io/github/v/release/DeadReDeamed/DraconiaBot" alt="releasestatus"/>
+
 
 # Draconia bot
 This is a discord bot created for a RPG experience in discord.
